bc217c13ee862c9a5791d4781278f49aeb4ef285
islam.ar <islam.ahmed@approcks.com>
2021-09-07 22:45:17 +0200

    Provide use cases dependencies

--- app/src/main/java/com/islam/recorder/di/AppModule.kt
+++ app/src/main/java/com/islam/recorder/di/AppModule.kt
@@ -5,6 +5,10 @@ import com.islam.data.db.AppDatabase
 import com.islam.data.repositories.*
 import com.islam.domain.repositories.MainRepository
 import com.islam.domain.repositories.RecordRepository
+import com.islam.domain.usecases.DeleteRecordUseCase
+import com.islam.domain.usecases.GetAllRecordUseCase
+import com.islam.domain.usecases.SaveRecordUseCase
+import com.islam.domain.usecases.UndoClipUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -42,6 +46,29 @@ object AppModule {
         return AppDatabase.invoke(appContext)
     }
 
+    @Singleton
+    @Provides
+    fun provideSaveRecordUseCase(mainRepository: MainRepository): SaveRecordUseCase {
+        return SaveRecordUseCase(mainRepository)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGetAllRecordUseCase(recordRepository: RecordRepository): GetAllRecordUseCase {
+        return GetAllRecordUseCase(recordRepository)
+    }
+
+    @Singleton
+    @Provides
+    fun provideDeleteRecordUseCase(recordRepository: RecordRepository): DeleteRecordUseCase {
+        return DeleteRecordUseCase(recordRepository)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUndoClipUseCase(recordRepository: RecordRepository): UndoClipUseCase {
+        return UndoClipUseCase(recordRepository)
+    }
 
 }
 
