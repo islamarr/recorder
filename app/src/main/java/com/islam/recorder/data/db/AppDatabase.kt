e0ded6df06a4ddcd77457838a369613a328c8580
islam.ar <islam.ahmed@approcks.com>
2021-09-10 10:11:00 +0200

    Remove Type converters in Room

--- app/src/main/java/com/islam/recorder/data/db/AppDatabase.kt
+++ app/src/main/java/com/islam/recorder/data/db/AppDatabase.kt
@@ -4,17 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.islam.recorder.data.db.daos.ClipDao
 import com.islam.recorder.data.db.entities.Clip
-import com.islam.recorder.generalUtils.TypeConvertersObject
 
 @Database(
     entities = [Clip::class],
     version = 1
 )
-
-@TypeConverters(TypeConvertersObject::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun getClipDao(): ClipDao
@@ -36,6 +32,6 @@ abstract class AppDatabase : RoomDatabase() {
                 context.applicationContext,
                 AppDatabase::class.java,
                 "App.db"
-            ).fallbackToDestructiveMigration().build()
+            ).build()
     }
 }
