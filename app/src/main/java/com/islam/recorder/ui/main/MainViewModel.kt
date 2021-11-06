292cbed1b0fdbeb527fafa0836bc88f35249b8af
islam.ar <islam.ahmed@approcks.com>
2021-11-06 21:11:24 +0200

    Pass useCases to viewModels

--- app/src/main/java/com/islam/recorder/ui/main/MainViewModel.kt
+++ app/src/main/java/com/islam/recorder/ui/main/MainViewModel.kt
@@ -2,15 +2,16 @@ package com.islam.recorder.ui.main
 
 import androidx.lifecycle.ViewModel
 import com.islam.recorder.domain.entites.Clip
-import com.islam.recorder.domain.repositories.MainRepository
+import com.islam.recorder.domain.usecases.SaveRecordUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
+class MainViewModel @Inject constructor(private val saveRecordUseCase: SaveRecordUseCase) :
+    ViewModel() {
 
     suspend fun saveRecord(clip: Clip) {
-        mainRepository.saveRecord(clip)
+        saveRecordUseCase.invoke(clip)
     }
 
 }
--- app/src/main/java/com/islam/recorder/ui/recordings/RecordingsViewModel.kt
+++ app/src/main/java/com/islam/recorder/ui/recordings/RecordingsViewModel.kt
@@ -3,24 +3,30 @@ package com.islam.recorder.ui.recordings
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.islam.recorder.domain.entites.Clip
-import com.islam.recorder.domain.repositories.RecordRepository
+import com.islam.recorder.domain.usecases.DeleteRecordUseCase
+import com.islam.recorder.domain.usecases.GetAllRecordUseCase
+import com.islam.recorder.domain.usecases.UndoClipUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class RecordingsViewModel @Inject constructor(private val recordRepository: RecordRepository) :
+class RecordingsViewModel @Inject constructor(
+    private val getAllRecordUseCase: GetAllRecordUseCase,
+    private val deleteRecordUseCase: DeleteRecordUseCase,
+    private val undoClipUseCase: UndoClipUseCase
+) :
     ViewModel() {
 
     suspend fun getAllRecord(): LiveData<MutableList<Clip>> {
-        return recordRepository.getAllRecord()
+        return getAllRecordUseCase.invoke()
     }
 
     suspend fun deleteRecord(clipId: Int) {
-        recordRepository.deleteRecord(clipId)
+        deleteRecordUseCase.invoke(clipId)
     }
 
     suspend fun undoClip(id: Int) {
-        recordRepository.undoClip(id)
+        undoClipUseCase.invoke(id)
     }
 
 }
